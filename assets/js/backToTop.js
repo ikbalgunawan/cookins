010dd262d11caa3140354311a628c067494a1949
ikbalgunawan <ikbalgunawan2122@gmail.com>
2022-03-09 08:04:11 +0700

    upload templated

---
+++ assets/js/backToTop.js
@@ -0,0 +1,14 @@
+var mybtn = document.getElementById("btn-top");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybtn.style.display = "block";
+    } else {
+        mybtn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
